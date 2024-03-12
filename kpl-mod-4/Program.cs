ae1b52faa410024f800b490d8bd09c80baccb319
Indra Mahesa <indramahesa128@gmail.com>
2024-03-12 21:57:38 +0700

    menambahkan implementasi dengan state-based construction

--- kpl-mod-4/Program.cs
+++ kpl-mod-4/Program.cs
@@ -26,10 +26,81 @@ internal class Program
         }
     }
 
+    public enum DoorState { TERKUNCI, TERBUKA };
+    public enum Trigger { KUNCI_PINTU, BUKA_PINTU };
+
+    public class DoorMachine
+    {
+        public DoorState currentState = DoorState.TERKUNCI;
+
+        public class Transition
+        {
+
+            public DoorState stateAwal;
+            public DoorState stateAkhir;
+            public Trigger trigger;
+            public Transition(DoorState stateAwal, DoorState stateAkhir, Trigger trigger)
+            {
+                this.stateAwal = stateAwal;
+                this.stateAkhir = stateAkhir;
+                this.trigger = trigger;
+            }
+        }
+
+        Transition[] transisi =
+        {
+            new Transition(DoorState.TERKUNCI, DoorState.TERKUNCI, Trigger.KUNCI_PINTU),
+            new Transition(DoorState.TERKUNCI, DoorState.TERBUKA, Trigger.BUKA_PINTU),
+            new Transition(DoorState.TERBUKA, DoorState.TERBUKA, Trigger.BUKA_PINTU),
+            new Transition(DoorState.TERBUKA, DoorState.TERKUNCI, Trigger.KUNCI_PINTU),
+        };
+
+        public DoorState GetNextState(DoorState stateAwal, Trigger trigger)
+        {
+            DoorState stateAkhir = stateAwal;
+
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+
+                if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+            return stateAkhir;
+        }
+
+        public void ActivateTrigger(Trigger trigger)
+        {
+
+            if (trigger == Trigger.BUKA_PINTU)
+            {
+                Console.WriteLine("Anda melakukan aksi buka pintu");
+            }
+            else
+            {
+                Console.WriteLine("Anda melakukan penguncian pintu");
+            }
+
+            currentState = GetNextState(currentState, trigger);
+
+            if (currentState == DoorState.TERBUKA)
+            {
+                Console.WriteLine("Pintu Anda Sekarang Terbuka!");
+            } else
+            {
+                Console.WriteLine("Pintu Anda Sekarang Terkunci!");
+            }
+        }
+    }
+
     public static void Main(string[] args)
     {
-        var kode = KodePos.getKodePos(Kelurahan.Batununggal);
-        Console.WriteLine(kode);
+        DoorMachine objMhs = new DoorMachine();
+        Console.WriteLine(objMhs.currentState);
+        objMhs.ActivateTrigger(Trigger.KUNCI_PINTU);
+        objMhs.ActivateTrigger(Trigger.BUKA_PINTU);
     }
 }
 
