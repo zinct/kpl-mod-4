e1df40ba121fc8329c7ade022dad03aa497cf89c
Indra Mahesa <indramahesa128@gmail.com>
2024-03-12 21:48:57 +0700

    menambahkan implementasi dengan table-driven

--- kpl-mod-4/Program.cs
+++ kpl-mod-4/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public enum Kelurahan
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+
+    class KodePos
+    {
+        public static int getKodePos(Kelurahan kelurahan)
+        {
+            int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return kodePos[(int)kelurahan];
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        var kode = KodePos.getKodePos(Kelurahan.Batununggal);
+        Console.WriteLine(kode);
+    }
+}
+
